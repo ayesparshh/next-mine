f37ba186fc686d2bc71ede589ac044b5d549850e
Ssparshgg <wasteid872@gmail.com>
2024-06-30 22:31:51 +0530

    auth not working

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,12 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { LatestInvoicesSkeleton ,  CardsSkeleton} from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const [ cardData] = await Promise.all([
-    fetchCardData(),
-  ]);
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
